e06646bfff5ffee4aac6feec160706a86dc437e3
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2022-05-27 14:04:18 +0530

    modified file

--- 27may22.py
+++ 27may22.py
@@ -1,1 +1,3 @@
 print("github repo")
+# and if theres any changes in this python file we will see it on github too as the repositories are uploaded on github via file pushing in git bash.
+print("agrim has modified this file")
