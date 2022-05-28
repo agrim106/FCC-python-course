9693a99d31640933838ba22e98bc83d834322c20
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2022-05-28 21:53:33 +0530

    added a new thing

--- Listfunctions.py
+++ Listfunctions.py
@@ -1,4 +1,9 @@
+import numbers
+
+
 lucky_numbers = [32,88,97,56,5,45,57,99,78]
 friends = ["mike","karen","peter","simon","barth","john", "david", "judas"]
 friends.sort()
 print(friends)
+lucky_numbers.sort()
+print(lucky_numbers)
